3a977388563dfc4978e3e91791dec4b1afef2f9f
Axel <>
2024-02-23 13:56:11 +0100

    Factorisation de la connexion à la base de données

--- controllers/note-controller.js
+++ controllers/note-controller.js
@@ -2,13 +2,21 @@ import mysql from 'mysql2/promise';
 
 export class NoteController {
 
-    async listAll(req, res) {
-        console.log('NoteController should list them all');
+    async dbConnection() {
+
         const dbConnection = await mysql.createConnection({
             host: '127.0.0.1',
             user: 'root',
             database: 'notes'
         });
+
+        return dbConnection;
+
+    }
+
+    async listAll(req, res) {
+        
+        const dbConnection = await this.dbConnection();
 
         console.log('connexion db reussie');
         const [results, fields] = await dbConnection.query('SELECT * from notes');
@@ -16,32 +24,23 @@ export class NoteController {
 
     }
 
-    async create(req, res){
+    async create(req, res) {
+
         console.log('note Controller create');
         const newNote = req.body;
         console.log('newNote :', newNote);
 
-        
-        const dbConnection = await mysql.createConnection({
-            host: '127.0.0.1',
-            user: 'root',
-            database: 'notes'
-        });
-
+        const dbConnection = await this.dbConnection();
         const [results, fields] = await dbConnection.query('INSERT INTO notes (text) VALUES (?)', [req.body.text]);
 
-        res.json({message: "note added to db"});
+        res.json({ message: "note added to db" });
     }
 
-    async destroy(req, res){
-        const dbConnection = await mysql.createConnection({
-            host: '127.0.0.1',
-            user: 'root',
-            database: 'notes'
-        });
-
+    async destroy(req, res) {
+    
+        const dbConnection = await this.dbConnection();
         const [results, fields] = await dbConnection.query('DELETE FROM notes where id = ?', [req.params.id]);
 
-        res.json({message: "note deleted", results});
+        res.json({ message: "note deleted", results });
     }
 }
